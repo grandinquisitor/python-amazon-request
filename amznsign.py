490b654a5c99aa865cb2d2bf25d4ca863310e7db
nick <infinityminusnine@gmail.com>
2011-06-05 11:24:16 -0400

    adding primary file

---
+++ amznsign.py
@@ -0,0 +1,46 @@
+import base64,hashlib,hmac,time
+import urllib
+ 
+# Add a ISO 8601 compliant timestamp (in GMT)
+ 
+def get_signed(url_params, access_key, secret):
+    # Sort the URL parameters by key
+
+    url_params['AWSAccessKeyId'] = access_key
+    url_params['Timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
+
+    keys = url_params.keys()
+    keys.sort()
+    # Get the values in the same order of the sorted keys
+    values = map(url_params.get, keys)
+     
+    # Reconstruct the URL paramters and encode them
+    url_string = urllib.urlencode( zip(keys,values) )
+    url_string = url_string.replace('+'," ") 
+    url_string = url_string.replace(':',":") 
+     
+    #Construct the string to sign
+    base_url = "http://ecs.amazonaws.com/onca/xml"
+    string_to_sign = "GET\necs.amazonaws.com\n/onca/xml\n%s" % url_string
+
+    h = hmac.new(secret, string_to_sign, hashlib.sha256)
+    signature = base64.b64encode(h.digest())
+    request = "%s?%s&Signature=%s" % (base_url,url_string,urllib.quote(signature))
+
+    return request
+
+if __name__ == '__main__':
+
+    AWS_ACCESS_KEY_ID = "Your Access Key ID"
+    AWS_SECRET_ACCESS_KEY = "Your Secret Key"
+ 
+    url_params = {
+        'Operation':"ItemSearch",
+        'Service':"AWSECommerceService",
+        'ItemPage':"1",
+        'ResponseGroup':"Images,ItemAttributes",
+        "SearchIndex":"Books",
+        'Keywords':"Monkees"
+    }
+ 
+    print get_signed(url_params,AWS_ACCCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
